895c6b7b852d213a1f4521218048d0cc1137bc00
KjartanKlein <kjartanbklein@gmail.com>
2020-07-28 11:28:57 +0000

    Test commit

---
+++ GPIO/target/arm-unknown-linux-gnueabihf/debug/deps/GPIO-097d298e3af55060.d
@@ -0,0 +1,5 @@
+/home/kjartan/examples/examples/GPIO/target/arm-unknown-linux-gnueabihf/debug/deps/GPIO-097d298e3af55060: src/main.rs
+
+/home/kjartan/examples/examples/GPIO/target/arm-unknown-linux-gnueabihf/debug/deps/GPIO-097d298e3af55060.d: src/main.rs
+
+src/main.rs:
